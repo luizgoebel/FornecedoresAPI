bafd50f369e6f8c5e45e1d717a55ed3331e8bb0b
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2024-06-25 00:47:07 -0300

    -Ajustando para popular o banco ao realizar o comando update-database.

--- Fornecedores.API/Program.cs
+++ Fornecedores.API/Program.cs
@@ -16,37 +16,34 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging(logging =>
 {
-    logging.ClearProviders(); 
-    logging.AddConsole(); // Adiciona o provedor de log para o console
+    logging.ClearProviders(); // Limpa os provedores de log existentes
+    logging.AddConsole();     // Adiciona o provedor de log para o console
 });
 
 builder.Services.AddDbContext<FornecedorDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-// Registro do serviço e do repositório
 builder.Services.AddScoped<IFornecedorService, FornecedorService>();
 builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiFornecedores", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_Fornecedores", Version = "v1" });
 });
+
 builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseSwagger();
-
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiFornecedores V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_Fornecedores V1");
 });
 
-// Configure o logging
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
-
-// Exemplo de log de informação
 logger.LogInformation("Aplicativo iniciado.");
 
 app.UseHttpsRedirection();
--- Fornecedores.Infrastructure/Migrations/20240624233724_PrimeiraMigration.cs
+++ Fornecedores.Infrastructure/Migrations/20240624233724_PrimeiraMigration.cs
@@ -23,6 +23,18 @@ namespace Fornecedores.Infrastructure.Migrations
                 {
                     table.PrimaryKey("PK_Fornecedores", x => x.Id);
                 });
+            // Popula o banco de dados com registros iniciais
+            migrationBuilder.InsertData(
+                table: "Fornecedores",
+                columns: new[] { "Nome", "Email" },
+                values: new object[,]
+                {
+                    { "Fornecedor 1", "fornecedor1@example.com" },
+                    { "Fornecedor 2", "fornecedor2@example.com" },
+                    { "Fornecedor 3", "fornecedor3@example.com" },
+                    { "Fornecedor 4", "fornecedor4@example.com" },
+                    { "Fornecedor 5", "fornecedor5@example.com" }
+                });
         }
 
         /// <inheritdoc />
